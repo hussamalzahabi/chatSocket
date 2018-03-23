3e4a272b0ae0cd9d8cf72936cd547f1bcadd756f
hz_php <hz_php@hotmail.com>
2018-03-23 18:43:04 -0400

    update

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+
+let app = express() ,
+    port = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname,"../public")));
+
+
+app.listen(port,()=>{
+    console.log(`Starting connecting in ${port} port`)
+});
